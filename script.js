8076adc4d639c418929c1e56c180435bf50c7ecc
RITIK JAIN <72255272+mrritikjain@users.noreply.github.com>
2025-12-15 08:08:53 +0530

    Phase1 of script is created

---
+++ script.js
@@ -0,0 +1,46 @@
+let input = document.querySelector("#input");
+let loading = document.querySelector(".loading");
+let results = document.querySelector(".results");
+
+function debounc(delay) {
+  let timer;
+  return function (callback) {
+    clearTimeout(timer);
+    timer = setTimeout(callback, delay);
+  };
+}
+let debouncingSearch = debounc(300);
+
+async function fetchUser(query) {
+  if (!query) {
+    results.innerHTML = "";
+    return;
+  }
+  try {
+    loading.innerHTML = "Loading...";
+    const res = await fetch(`https://api.github.com/search/users?q=${query}`);
+    const data = await res.json();
+    renderResults(data.items);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    loading.innerHTML = "";
+  }
+}
+
+function renderResults(users) {
+  results.innerHTML = "";
+
+  users.slice(0, 10).forEach((user) => {
+    const li = document.createElement("li");
+    li.innerText = user.login;
+    results.appendChild(li);
+  });
+}
+input.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  debouncingSearch(() => {
+    fetchUser(value);
+  });
+});
